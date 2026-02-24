4707a5a1adbc27218dc578f33fb35c0bb327981b
steve-caiula <caiulastefano@gmail.com>
2026-02-24 11:56:36 +0100

    hw: complete KiCad schematic

--- include/pin_definitions.h
+++ include/pin_definitions.h
@@ -10,8 +10,8 @@
 #define TEMP_SENSOR PD3
 
 // LCD PINS
-#define LCD_RS PB0
-#define LCD_E PB1
+#define LCD_RS  PB0
+#define LCD_E   PB1
 #define LCD_DB4 PB2
 #define LCD_DB5 PB3
 #define LCD_DB6 PB4
